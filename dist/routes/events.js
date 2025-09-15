99c0d20452263ff4cce95e5af571e6d6499b19e4
Muhammad Umer Adeem <adeemumer800@gmail.com>
2025-09-15 15:50:23 +0500

    eventsphere portal

--- dist/routes/events.js
+++ dist/routes/events.js
@@ -3,10 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
+const express_1 = require("express");
 const db_1 = __importDefault(require("../db"));
 const authMiddleware_1 = require("../middleware/authMiddleware");
-const router = express_1.default.Router();
+const router = express_1.Router();
 // GET /api/events
 router.get('/', async (req, res) => {
     try {
